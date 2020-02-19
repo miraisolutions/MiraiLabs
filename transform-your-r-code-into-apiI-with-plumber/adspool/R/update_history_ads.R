dd0b9e80a8f89dcae3f1092f1536fc6c84d8c84a
Gustavo Martinez <gustavo.martinez@mirai-solutions.com>
2020-02-19 17:36:07 +0100

    Added update_history_ads.R

---
+++ transform-your-r-code-into-apiI-with-plumber/adspool/R/update_history_ads.R
@@ -0,0 +1,10 @@
+#' update_history_ads
+#' @description
+#' Update the history of the ads shown.
+#' @param ads.history List containing the history of ads of a particular session.
+#'
+#' @return Side effect only.
+#' @export
+update_history_ads <- function(ads.history) {
+    return(invisible())
+}
